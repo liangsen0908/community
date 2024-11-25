0edd37cf5877f6513b7ac4704bc9b02b47b78bc2
liangsen0908 <1764792631@qq.com>
2024-11-25 19:03:07 +0800

    community

---
+++ src/main/java/com/complain/community/service/UserService.java
@@ -0,0 +1,18 @@
+package com.complain.community.service;
+
+import com.complain.community.dao.UserMapper;
+import com.complain.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+
+}
